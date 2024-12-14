f2435dc3caf169f03240e2c415121fe3a59195eb
Santosh Ghodekar <sghodekar@gmail.com>
2024-12-14 18:30:54 +0530

    initial setup

---
+++ src/test/java/com/trailblaze/adventureworks/AdventureworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trailblaze.adventureworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
